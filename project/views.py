1a4f8c221d3cb567bceca8a049c93544b9c475d9
briandoyle89 <briand@classavo.com>
2018-02-18 20:49:38 -0500

    First Things

--- project/views.py
+++ project/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from project.models import course, user, file
+
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+
+def index(request):
+    courses = course.objects.all()
+    return render(request, 'project/index.html', courses)
