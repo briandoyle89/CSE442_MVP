2c983bc5b51fe522844e4355cca4c54fb5415d08
briandoyle89 <briand@classavo.com>
2018-03-10 22:11:51 -0500

    Login + Register and security in the settings ready  to go.

--- project/urls.py
+++ project/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     url('', views.index, name='index'),
 
     url(r'^$', views.index, name='index'),
-    url(r'^$login', views.login, name='login'),
+    url(r'^login$', views.login, name='login'),
     url(r'^register$', views.register, name='register'),
-    url(r'^check_register', views.check_register, name='check_register'),
-    url(r'^course_list$', views.course_list, name='course_list'),
-    url(r'^(?P<identity>[0-9a-zA-Z]+)/course_view$', views.course_view, name='course_view')
+    url(r'^check_register$', views.check_register, name='check_register'),
+    url(r'^course_view$', views.course_view, name='course_view'),
+    url(r'^file_upload$', views.file_upload, name='file_upload'),
 ]
