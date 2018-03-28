6cc926b003446e9780a903a46be7e3cc13cddd05
briandoyle89 <31046077+briandoyle89@users.noreply.github.com>
2018-03-28 12:25:08 -0400

    Update settings.py

--- CSE_442/settings.py
+++ CSE_442/settings.py
@@ -68,13 +68,13 @@ TEMPLATES = [
     },
 ]
 
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-]
+# PASSWORD_HASHERS = [
+#     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+#     'django.contrib.auth.hashers.BCryptPasswordHasher',
+#     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+#     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+#     'django.contrib.auth.hashers.Argon2PasswordHasher',
+# ]
 
 WSGI_APPLICATION = 'CSE_442.wsgi.application'
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
