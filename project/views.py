af3dab38114a39e467214f40cba9e972b9914703
briandoyle89 <31046077+briandoyle89@users.noreply.github.com>
2018-05-01 13:30:24 +0100

    Update views.py

--- project/views.py
+++ project/views.py
@@ -10,6 +10,8 @@ from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+import re
 
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
@@ -30,10 +32,6 @@ def login_user(request):
             login(request, this_user)
             print(request.user)
             return redirect(course_list)
-            # return render(request, 'project/index.html', {'error_message': "Incorrect Username or Password."})
-        # for users in user.objects.all():
-        #     if username == users.user_name and password == users.password:
-        #         return redirect(course_list)
         else:
             return render(request, 'project/index.html', {'error_message': "Please Log In."})
     return render(request, 'project/index.html')
@@ -47,12 +45,23 @@ def check_register(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         email = request.POST.get("email")
-        user = User.objects.create_user(username=username,
-                            password=password,
-                            email=email)
-        user.save()
-        user.backend = 'django.contrib.auth.backends.ModelBackend'
-        return redirect(index)
+        print(str(len(username)))
+        if len(username) < 3:
+            return render(request, 'project/index.html', {
+                'message': "Username not valid, must be three characters, letters, numbers and underscore and start with a letter or number."})
+        if not re.match('^[A-Za-z0-9]+[A-Za-z0-9_]+[A-Za-z0-9_]+$', username):
+            return render(request, 'project/index.html', {
+                'message': "Username not valid, must be three characters, letters, numbers and underscore and start with a letter or number."})
+
+        try:
+            user = User.objects.create_user(username=username,
+                                password=password,
+                                email=email)
+            user.save()
+            user.backend = 'django.contrib.auth.backends.ModelBackend'
+            return redirect(index)
+        except IntegrityError:
+            return render(request, 'project/index.html', {'message': "Username Already Exists."})
 
 #View list of courses after login.
 @login_required
@@ -60,7 +69,6 @@ def course_list(request):
 
     ##if not user redirect to index w/ message need to sign in
     courses = course.objects.all()
-    for course_ in courses: print(course_)
     user = request.user
     return render(request, 'project/dashboard.html', {'courses': courses,
                                                       'user': user})
@@ -71,25 +79,36 @@ def course_view(request, identity):
     if request.method == "POST":
         course_name = request.POST.get("course_name")
         files = file.objects.filter(course=course.objects.get(course_name=course_name))
+        user_votes = UserVotes.objects.all()
         user = request.user
         return render(request, 'project/class_view.html', {'files': files,
                                                            'course_name': course_name,
+                                                           'user_votes': user_votes,
                                                            'user': user
                                                            })
-#Rest are self-explanotary
+#Rest are self-explanotary4
 @login_required
 def file_upload(request):
     this_user = request.user
 
     if request.method == 'POST':
         form = FileForm(request.POST, request.FILES)
+        print("here2")
         if form.is_valid():
             fileuploading = form.save(commit=False)
             fileuploading.username = request.user
-
-            fileuploading.save()
-            courses = course.objects.all()
-            return render(request, 'project/dashboard.html', {'courses': courses})
+            file_name = fileuploading.file_name
+            print("here1")
+            if re.match('^[A-Za-z0-9]+[A-Za-z0-9_\s]+[A-Za-z0-9_]+$', file_name):
+                print("here")
+                fileuploading.save()
+                courses = course.objects.all()
+                return render(request, 'project/dashboard.html', {'courses': courses})
+            else:
+                return render(request, 'project/file_upload.html', {'form': form,
+                                                                    'message': "Please enter a valid file name."})
+        else:
+            return render(request, 'project/file_upload.html', {'form': form})
     else:
         form = FileForm()
         return render(request, 'project/file_upload.html', {'form': form})
@@ -116,28 +135,33 @@ def download(request, file_chosen):
 def upvote(request, file_chosen):
     course_name = request.POST.get('course_is')
     file_chosen = file.objects.get(id=file_chosen)
+    user_votes = UserVotes.objects.all()
     files = file.objects.all()
     file_chosen.upvote(request.user)
     return render(request, 'project/class_view.html', {'files': files,
                                                        'course_name': course_name,
-                                                       'user': user
+                                                       'user_votes': user_votes,
+                                                       'user': request.user
                                                        })
 
 @login_required()
 def downvote(request, file_chosen):
     course_name = request.POST.get('course_is')
     file_chosen = file.objects.get(id=file_chosen)
+    user_votes = UserVotes.objects.all()
     files = file.objects.all()
     file_chosen.downvote(request.user)
     return render(request, 'project/class_view.html', {'files': files,
                                                        'course_name': course_name,
-                                                       'user': user
+                                                       'user_votes': user_votes,
+                                                       'user': request.user
                                                        })
 
 @login_required
 def my_uploads(request):
     this_user = request.user
     files = file.objects.all()
+    user_votes = UserVotes.objects.all()
     file_list = []
     print(this_user)
     for file_object in files:
@@ -147,6 +171,7 @@ def my_uploads(request):
             file_list.append(file_object)
 
     return render(request, 'project/myuploads.html', {'file_list': file_list,
+                                                        'user_votes': user_votes,
                                                       'user': this_user
                                                       })
 
@@ -154,9 +179,15 @@ def my_uploads(request):
 def file_search(request):
     if request.method == 'POST':
         search_term = request.POST.get("search")
+        user_votes = UserVotes.objects.all()
         similar_files = file.objects.filter(file_name__contains=search_term)
+        if len(similar_files) == 0:
+            return render(request, 'project/file_search.html', {'similar_files': similar_files,
+                                                                'user_votes': user_votes,
+                                                                'no_files': "There are no matching files."})
 
-        return render(request, 'project/file_search.html', {'similar_files': similar_files})
+        return render(request, 'project/file_search.html', {'similar_files': similar_files,
+                                                        'user_votes': user_votes})
 
     else:
         courses = course.objects.all()
@@ -164,12 +195,11 @@ def file_search(request):
                                                           'user': request.user
                                                           })
 
-
-
 @login_required
 def my_downloads(request):
     this_user = request.user
     downloaded_files = downloaded_file.objects.all()
+    user_votes = UserVotes.objects.all()
     file_list = []
     for dl_file in downloaded_files:
         if dl_file.username == this_user:
@@ -177,6 +207,7 @@ def my_downloads(request):
             file_list.append(this_file)
 
     return render(request, 'project/mydownloads.html', {'file_list': file_list,
+                                                        'user_votes': user_votes,
                                                         'user': this_user
                                                         })
 
